866b9b3e381ab71b6923a4e694aeb64474ab62b0
Frank <Frank@FrankNote>
2019-05-06 20:26:32 -0400

    Criação e mapeamento dos atributos de classe e hibernate config

--- src/java/br/com/locadora/mapeamento/pessoa/Pessoa.java
+++ src/java/br/com/locadora/mapeamento/pessoa/Pessoa.java
@@ -5,12 +5,58 @@
  */
 package br.com.locadora.mapeamento.pessoa;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
 /**
  *
  * @author Frank
  */
+@MappedSuperclass
 public abstract class Pessoa {
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(length = 100, nullable = false)
     private String nome;
+    @Column(nullable = false)
     private int idade;
-    private String cpf;
+    @Column(length = 15, nullable = false)
+    private String cpfCNPJ;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCpfCNPJ() {
+        return cpfCNPJ;
+    }
+
+    public void setCpfCNPJ(String cpfCNPJ) {
+        this.cpfCNPJ = cpfCNPJ;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    
+    
 }
