e2fb289b7d19923d18cc5ff6df98901521bf075f
Frank <Frank@FrankNote>
2019-06-01 22:35:52 -0400

    add configurcao de navegacao, cadastro cliente

---
+++ src/java/br/com/locadora/controller/ClienteController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.locadora.controller;
+
+import javax.faces.bean.ManagedBean;
+import org.hibernate.SessionFactory;
+
+/**
+ *
+ * @author Frank
+ */
+@ManagedBean
+public class ClienteController {
+    
+    public void salvar(){
+        //SessionFactory sf = NewHibernateUtil.getSessionFactory();
+        
+    }
+}
